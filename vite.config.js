5045ff3c0c21240d3870c253de96a2932181b2d5
pushpender0812 <yadavpushpender6378@gmail.com>
2024-11-08 10:57:26 +0530

    Converted to react-native

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',  // Automatically updates service worker
+      injectRegister: 'auto',       // Automatically injects service worker registration
+      devOptions: {
+        enabled: true,              // Enable PWA service worker in development
+      },
+      manifest: {
+        name: 'Sarkari Exam Result',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: './assets/logo.jpg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './assets/logo.jpg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
